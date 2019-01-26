49c4dd21c63ded9eb430b4f372223d9f92797e81
Antonio Harger Rivero <antonio.harger@gmail.com>
2019-01-26 12:34:32 -0300

    Made character move and camera follow them

---
+++ Assets/AntonioHR/HomeColors/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AntonioHR.HomeColors
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        private CharacterController charController;
+
+        [SerializeField]
+        private float speed = 5;
+        
+        void Awake()
+        {
+            charController = GetComponent<CharacterController>();
+        }
+        
+        void Update()
+        {
+            charController.Move(InputAsMoveAxis * speed);
+        }
+
+        public Vector3 InputAsMoveAxis
+        {
+            get
+            {
+                var rotation = Quaternion.AngleAxis(45, Vector3.up);
+                Vector3 vecBeforeRot = Vector3.forward * Input.GetAxis("Vertical") + Vector3.right * Input.GetAxis("Horizontal");
+                vecBeforeRot.Normalize();
+                return rotation * vecBeforeRot;
+            }
+        }
+    }
+}
---
+++ Assets/AntonioHR/Util/Scripts/SimpleFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+
+    public void LateUpdate()
+    {
+        if (target != null)
+            GoToTarget();
+    }
+
+    private void GoToTarget()
+    {
+        transform.position = target.TransformPoint(Vector3.zero);
+    }
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+}
