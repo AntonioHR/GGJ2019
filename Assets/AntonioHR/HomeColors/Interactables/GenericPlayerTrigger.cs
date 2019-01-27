0508f845bfab8bd04febf75fcad2ee8979b043c3
Antonio Harger Rivero <antonio.harger@gmail.com>
2019-01-27 10:00:28 -0300

    Added damage section

---
+++ Assets/AntonioHR/HomeColors/Interactables/GenericPlayerTrigger.cs
@@ -0,0 +1,24 @@
+﻿using AntonioHR.HomeColors.PlayerBehaviours;
+using AntonioHR.Interactables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace AntonioHR.HomeColors.Interactables
+{
+    public class GenericPlayerTrigger : ObjectTrigger<Player>
+    {
+
+        [SerializeField]
+        private UnityEvent trigger;
+
+        protected override void OnTriggered(Player obj)
+        {
+            trigger.Invoke();
+        }
+    }
+}
--- Assets/AntonioHR/Util/Scripts/SimpleMove.cs
+++ Assets/AntonioHR/Util/Scripts/SimpleMove.cs
@@ -8,15 +8,22 @@ using UnityEngine.Events;
 
 public class SimpleMove : MonoBehaviour
 {
+
+
     [SerializeField]
     private bool triggerAtStart;
+    [SerializeField]
+    private bool loop = false;
     [SerializeField]
     private Transform targetRef;
     [SerializeField]
     private float time = .5f;
     [SerializeField]
+    private Ease individualEase = Ease.OutQuad;
+    [SerializeField]
+    private Ease totalEase = Ease.Linear;
+    [SerializeField]
     private UnityEvent OnFinished;
-
 
     void Start()
     {
@@ -32,19 +39,32 @@ public class SimpleMove : MonoBehaviour
         var rot = targetRef.transform.rotation;
 
         var seq = DOTween.Sequence();
-        seq.Insert(0, transform.DOMove(pos, time));
-        seq.Insert(0, transform.DORotateQuaternion(rot, time));
+        seq.Insert(0, transform.DOMove(pos, time)).SetEase(individualEase);
+        seq.Insert(0, transform.DORotateQuaternion(rot, time)).SetEase(individualEase);
+        seq.SetEase(totalEase);
         seq.OnComplete(OnFinished.Invoke);
+
+        if (loop)
+            seq.SetLoops(-1, LoopType.Yoyo);
     }
 
     [Button]
     private void CreateChildForRef()
     {
-        var obj = new GameObject();
+        var obj = new GameObject("Simple Move Target");
         obj.transform.parent = transform;
         obj.transform.localScale = Vector3.one;
         obj.transform.localPosition = Vector3.zero;
         obj.transform.rotation = Quaternion.identity;
         targetRef = obj.transform;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (targetRef == null)
+            return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(targetRef.transform.position, .05f);
+        Gizmos.DrawLine(transform.position, targetRef.transform.position);
     }
 }
